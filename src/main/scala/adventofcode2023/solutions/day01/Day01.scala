00e1629ab740a640900325ee89a91d32ac3349e7
astrionic <32393038+astrionic@users.noreply.github.com>
2023-12-01 17:32:10 +0100

    Solve day 1

---
+++ src/main/scala/adventofcode2023/solutions/day01/Day01.scala
@@ -0,0 +1,68 @@
+package space.astrionic
+package adventofcode2023.solutions.day01
+
+import adventofcode2023.framework.{AdventSolution, matchRegex, toIntOption}
+
+object Day01 extends AdventSolution {
+
+  writeSolution = false
+  executePart = ExecutePart.Both
+
+  override def solvePart1(input: String): String = {
+    val regex = raw"\D*(\d)".r.unanchored
+
+    def firstDigitOf: String => Option[String] = matchRegex(regex)
+
+    def firstAndLastDigitOf(s: String): Option[String] = (firstDigitOf(s), firstDigitOf(s.reverse)) match {
+      case (Some(first), Some(last)) => Some(s"${first}${last}")
+      case _                         => None
+    }
+
+    input
+      .split("\n")
+      .flatMap(firstAndLastDigitOf)
+      .flatMap(toIntOption)
+      .sum
+      .toString
+  }
+
+  override def solvePart2(input: String): String = {
+    val digitMap = Map(
+      "one" -> "1",
+      "two" -> "2",
+      "three" -> "3",
+      "four" -> "4",
+      "five" -> "5",
+      "six" -> "6",
+      "seven" -> "7",
+      "eight" -> "8",
+      "nine" -> "9"
+    )
+
+    val regex = s"(\\d|${digitMap.keys.mkString("|")})".r.unanchored
+    def firstDigitOf: String => Option[String] = matchRegex(regex)
+
+    val digitStringsReverse = digitMap.keys.map(_.reverse).mkString("|")
+    val regexReverse = s"(\\d|${digitStringsReverse})".r.unanchored
+    def lastDigitOf(s: String): Option[String] = matchRegex(regexReverse)(s.reverse).map(_.reverse)
+
+    def wordToDigit(s: String): String = {
+      digitMap.get(s) match {
+        case Some(digit) => digit
+        case None        => s
+      }
+    }
+
+    def firstAndLastDigitOf(s: String): Option[String] = (firstDigitOf(s), lastDigitOf(s)) match {
+      case (Some(first), Some(last)) => Some(s"${wordToDigit(first)}${wordToDigit(last)}")
+      case _                         => None
+    }
+
+    input
+      .split("\n")
+      .flatMap(firstAndLastDigitOf)
+      .flatMap(toIntOption)
+      .sum
+      .toString
+  }
+}
